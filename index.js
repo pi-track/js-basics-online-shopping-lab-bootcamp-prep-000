6ad66189c6b8bf974e53706d0c6ebf742d86c6fc
Patrick Eells <patrick.eells@gmail.com>
2017-12-05 21:07:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ function total() {
 
 function removeFromCart(item) {
   var entered = false
-  console.log(Object.keys(cart[0]))
   for (var i=0;i<cart.length;i++) {
     if (Object.keys(cart[i])===item) {
       cart.splice(i,1)

119b4998d290a96ec5cbcbbb4e5e5cc2d3ca8db6
Patrick Eells <patrick.eells@gmail.com>
2017-12-05 21:55:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
     var totalCost = total()
-    console.log(`Your total cost is ${total}, which will be charged to the card ${cardNumber}.`)
+    console.log(`Your total cost is ${totalCost}, which will be charged to the card ${cardNumber}.`)
     setCart[[]]
   }
   return

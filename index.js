c50c4e16bb8ea5df85590c4777aa4f506dd23b6d
Patrick Eells <patrick.eells@gmail.com>
2017-12-05 21:41:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,6 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  console.log('here i am')
   if (cardNumber === '') {
     console.log("Sorry, we don\'t have a credit card on file for you.")
   }

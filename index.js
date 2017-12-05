4ad06a1560688b2a229496bdb9f0f67f86308798
Patrick Eells <patrick.eells@gmail.com>
2017-12-05 21:48:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === 0) {
-    console.log("Sorry, we don\'t have a credit card on file for you.")
+  if (cardNumber === undefined) {
+    console.log("Sorry, we don't have a credit card on file for you.")
   }
 
   return

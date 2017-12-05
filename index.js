c3f98103c11620eb6f2a19246108e6f5e376b403
Patrick Eells <patrick.eells@gmail.com>
2017-12-05 20:33:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ function viewCart() {
   if (cart.length === 0) {
     console.log('Your shopping cart is empty.')
   }
-  else if (cart.length===1) {
+  else if (cart.length === 1) {
     var key = Object.keys(cart[0])
     var value = cart[0][key]
-    console.log(`In your cart, you have ${key} at $${cart[0]}.`)
+    console.log(`In your cart, you have ${key} at $${value}.`)
     return
   }
 }

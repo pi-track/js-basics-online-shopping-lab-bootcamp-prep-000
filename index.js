27295972aa3dd7620eededa6c1968a4758f7b01c
Patrick Eells <patrick.eells@gmail.com>
2017-12-05 21:50:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,6 +79,9 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
+  else {
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+  }
 
   return
 }

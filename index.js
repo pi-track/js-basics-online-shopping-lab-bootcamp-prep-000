c1536e04ae75c672abc0911a15a67fa1c6390b94
Patrick Eells <patrick.eells@gmail.com>
2017-12-05 21:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function placeOrder(cardNumber) {
   }
   else {
     var totalCost = total()
-    console.log(`Your total cost is ${total}, which will be charged to the card ${cardNumber}.`
+    console.log(`Your total cost is ${total}, which will be charged to the card ${cardNumber}.`)
   }
   return
 }
